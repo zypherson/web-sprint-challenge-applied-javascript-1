1be3646d64f0ff25c622052b69c8848ad45ab99c
zypherson <taylorzypherson@gmail.com>
2022-03-11 19:36:08 -0500

    updated sprint

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,12 @@
+
+import { topics } from "../mocks/data"
+
+const entryPoint = document.querySelector('.header-container')
+
+
 const Header = (title, date, temp) => {
+
+  
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -11,15 +19,45 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  const header = document.createElement('div')
+  const dateP = document.createElement('span')
+  const titleP = document.createElement('h1')
+  const tempP = document.createElement('span')
+  
+  header.classList.add('header')
+  dateP.classList.add('date')
+  tempP.classList.add('temp')
+
+  header.appendChild(dateP)
+  header.appendChild(titleP)
+  header.appendChild(tempP)
+
+  titleP.textContent= `${title}`
+  dateP.textContent= `${date}`
+  tempP.textContent= `${temp}`
+ 
+return header
+
 }
 
 const headerAppender = (selector) => {
+  const entryPoint = document.querySelector(selector)
+  const header = Header()
+  entryPoint.appendChild(header)
+    
+
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+
+  
+
+ 
+  
 }
 
 export { Header, headerAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import { topics } from "../mocks/data"
+const entryPoint = document.querySelector('.tabs-container')
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,8 +15,34 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-}
 
+  
+
+const topic = document.createElement('div')
+const tab1 = document.createElement('div')
+const tab2 = document.createElement('div')
+const tab3 = document.createElement('div')
+
+topic.classList.add('topics')
+tab1.classList.add('tab')
+tab2.classList.add('tab')
+tab3.classList.add('tab')
+
+topic.appendChild(tab1)
+topic.appendChild(tab2)
+topic.appendChild(tab3)
+
+tab1.textContent = topics[0]
+tab2.textContent = topics[1]
+tab3.textContent = topics[2]
+
+
+
+return topic
+ 
+
+}
+console.log(topics.topics[4])
 const tabsAppender = (selector) => {
   // TASK 4
   // ---------------------
@@ -23,6 +51,14 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  const entryPoint = document.querySelector(selector)
+  topics.topics.forEach(elem => {
+    const topic = Tabs()
+    entryPoint.appendChild(topic)
+   
+ })
+
+  
 }
 
 export { Tabs, tabsAppender }
